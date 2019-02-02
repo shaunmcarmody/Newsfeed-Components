5d7d21156c3ea93209e5303e84b2a64581e91b5f
c87 <shaun.carmody@live.co.uk>
2019-02-02 14:48:30 +0000

    add current date to new articles

--- Form/Form.js
+++ Form/Form.js
@@ -1,11 +1,17 @@
 document.querySelector('.form-button').addEventListener('click', (e) => {
     e.preventDefault();
+    const d = new Date();
+    
     const data = {
         header: document.querySelector('.form-title').value,
-        date: 'January 30th 2018',
+        date: `
+            ${d.toLocaleDateString('en-GB', { month: 'short' })}
+            ${d.getDay()}th,
+            ${d.getFullYear()}
+            `,
         article: document.querySelector('.form-article').value,
     }
     document.querySelector('.form-title').value = null;
     document.querySelector('.form-article').value = null;
     populateArticle(data);
-})
+});
